451db44051941c71763cc97c0b58fa94c35b1a63
FARmer037 <mustofa0367@gmail.com>
2019-02-14 12:35:23 +0700

    add ldr

---
+++ esp8266/ldr_module/ldr_module.ino
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+
+ESP8266WiFiMulti WiFiMulti;
+HTTPClient http;
+
+int analogPin = A0;
+int val = 0;
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+  Serial.println();
+  Serial.println("Connecting...");
+  WiFiMulti.addAP("AndroidAP", "fnei9721");
+
+}
+
+void loop() {
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+    val = analogRead(analogPin);
+    Serial.println(val);
+
+//  https://api.thingspeak.com/update?api_key=12GK5TRNPM50F9M5&field1=0
+    http.begin("http://api.thingspeak.com/update?api_key=12GK5TRNPM50F9M5&field1=" + String(val));
+
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+      if (httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+    }
+    else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+    http.end();
+  }
+  delay(20000);
+}
